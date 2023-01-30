c5f1b1391ffdad995e37b17cc0fe72b323ae679b
leesunghee <realwunz@gmail.com>
2023-01-30 23:26:57 +0900

    setState

--- lib/main.dart
+++ lib/main.dart
@@ -1,154 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:webtoon/widgets/button.dart';
-import 'package:webtoon/widgets/currency_card.dart';
 
 void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  List<int> numbers = [];
+
+  void onClicked() {
+    setState(() {
+      numbers.add(numbers.length);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          backgroundColor: const Color(0xFF181818),
-          body: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 80,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          const Text(
-                            'Hey, Selena',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 28,
-                              fontWeight: FontWeight.w800,
-                            ),
-                          ),
-                          Text(
-                            'Welcome back',
-                            style: TextStyle(
-                              color: Colors.white.withOpacity(0.8),
-                              fontSize: 18,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 70,
-                  ),
-                  Text(
-                    'Total Balance',
-                    style: TextStyle(
-                      color: Colors.white.withOpacity(0.8),
-                      fontSize: 22,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Text(
-                    '\$5 194 482',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 44,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Button(
-                        text: 'Transfer',
-                        backgroundColor: Colors.amber,
-                        textColor: Colors.black,
-                      ),
-                      Button(
-                        text: 'Request',
-                        backgroundColor: Color(0xFF1F2123),
-                        textColor: Colors.white,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      const Text(
-                        'Wallets',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 36,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Text(
-                        'View All',
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.8),
-                          fontSize: 18,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const CurrencyCard(
-                    name: 'Euro',
-                    code: 'EUR',
-                    amount: '6 428',
-                    icon: Icons.euro_rounded,
-                    isInverted: false,
-                  ),
-                  Transform.translate(
-                    offset: const Offset(
-                      0, -20,
-                    ),
-                    child: const CurrencyCard(
-                      name: 'Dollar',
-                      code: 'USD',
-                      amount: '428',
-                      icon: Icons.attach_money_rounded,
-                      isInverted: true,
-                    ),
-                  ),
-                  Transform.translate(
-                    offset: const Offset(
-                      0, -40,
-                    ),
-                    child: const CurrencyCard(
-                      name: 'Euro',
-                      code: 'EUR',
-                      amount: '6 428',
-                      icon: Icons.euro_rounded,
-                      isInverted: false,
-                    ),
-                  ),
-                ],
+        home: Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Click Count',
+              style: TextStyle(fontSize: 30),
+            ),
+            for(var n in numbers) Text('$n'),
+            IconButton(
+              iconSize: 40,
+              onPressed: onClicked,
+              icon: const Icon(
+                Icons.add_box_rounded,
               ),
             ),
-          )),
-    );
+          ],
+        ),
+      ),
+    ));
   }
 }
