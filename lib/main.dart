d2a624e4afd3b6c7f37120724839409f26acd2d9
leesunghee <realwunz@gmail.com>
2023-01-30 23:40:03 +0900

    BuildContext

--- lib/main.dart
+++ lib/main.dart
@@ -21,26 +21,39 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Click Count',
-              style: TextStyle(fontSize: 30),
-            ),
-            for(var n in numbers) Text('$n'),
-            IconButton(
-              iconSize: 40,
-              onPressed: onClicked,
-              icon: const Icon(
-                Icons.add_box_rounded,
-              ),
-            ),
-          ],
+        theme: ThemeData(
+            textTheme: const TextTheme(
+                titleLarge: TextStyle(
+                  color: Colors.red,
+                )
+            )
         ),
+        home: Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                MyLargeTitle(),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
-    ));
+    );
   }
 }
